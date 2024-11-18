093e6e44ec9fc8a5694ceea234a1774bebb8b990
angus-chanan11 <angus.chan@oocl.com>
2024-11-18 16:21:57 +0800

    [MultiplicationTableBuilder] feat: create isStartLessThanOrEqualToEnd function

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -6,12 +6,15 @@ public class MultiplicationTableBuilder {
         MultiplicationTableBuilder builder = new MultiplicationTableBuilder();
         int start = 2;
         int end = 4;
-        String multiplicationTable = builder.build(start, end);
-
-        System.out.println(multiplicationTable);
+        boolean isStartLessThanOrEqualToEnd = builder.isStartLessThanOrEqualToEnd(start, end);
+        System.out.println(isStartLessThanOrEqualToEnd);
     }
 
     public String build(int start, int end) {
         return "";
+    }
+
+    private boolean isStartLessThanOrEqualToEnd(int start, int end) {
+        return start <= end;
     }
 }
